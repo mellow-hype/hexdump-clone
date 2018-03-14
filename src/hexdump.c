d34983fcc7181f2fc13740b5900412c7500c8e06
mellow-hype <enrique.castillox@gmail.com>
2018-03-14 14:47:10 -0700

    Added hexdump() function that handles logic of iterating through 16-byte chunks

--- src/hexdump.c
+++ src/hexdump.c
@@ -1,11 +1,86 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define CHUNK_SIZE 0x10
 
-int hexdump(char * byte_buffer)
+unsigned long get_file_size(FILE *file_ptr);
+int hexdump(FILE *f_buffer);
+
+
+int hexdump(FILE *f_buffer)
 {
+    unsigned long f_size = get_file_size(f_buffer);
+    unsigned int chunks = (f_size / CHUNK_SIZE);
+    unsigned int leftover_chunk_size = (f_size % CHUNK_SIZE);
+    unsigned int chunk_seg;
+    
+    unsigned int c = 0;
+    while (c < chunks)
+    {
+        //  Calculate the offset into the file
+        chunk_seg = c * CHUNK_SIZE;
+        c++;
+        
+        // Allocate memory and ensure there were no errors
+        unsigned char *chunk_buf = NULL;
+        chunk_buf = (unsigned char *)malloc(((CHUNK_SIZE)*sizeof(unsigned char)));
 
-    return 0;
+        if (!chunk_buf)
+        {
+            printf("Allocation of memory failed\n");
+            return 0;
+        }
+
+        // Read bytes into the newly allocated buffer
+        fread(chunk_buf, 1, CHUNK_SIZE, f_buffer);
+        
+        // Print the file offset and begin looping through the bytes in the buffer
+        printf("0x%08x:| ", chunk_seg);
+        for (int i = 0; i < CHUNK_SIZE; i+=2)
+        {
+            unsigned char a, b;
+            a = (unsigned char)chunk_buf[i];
+            b = (unsigned char)chunk_buf[i+1];
+            
+            printf("%02x%02x ", a, b);
+        }
+
+        printf("\n");
+        free(chunk_buf);
+
+    }
+
+    if (leftover_chunk_size)
+    {
+        //  Calculate the offset into the file
+        chunk_seg = c * CHUNK_SIZE;
+
+        // Allocate memory and ensure there were no errors
+        unsigned char *chunk_buf = NULL;
+        chunk_buf = (unsigned char *)malloc(((leftover_chunk_size)*sizeof(unsigned char)));
+
+        if (!chunk_buf)
+        {
+            printf("Allocation of memory failed\n");
+            return 0;
+        }
+
+        fread(chunk_buf, 1, CHUNK_SIZE, f_buffer);
+        printf("0x%08x:| ", chunk_seg);
+        for (int i = 0; i < leftover_chunk_size; i+=2)
+        {
+            unsigned char a, b;
+            a = (unsigned char)chunk_buf[i];
+            b = (unsigned char)chunk_buf[i+1];
+            printf("%02x%02x ", a, b);
+        }
+        printf("\n");
+        free(chunk_buf);
+
+    }
+
+    printf("[+] File size: %lu\n", f_size);
+    return 1;
 }
 
 // Calculate the file size
@@ -16,7 +91,6 @@ unsigned long get_file_size(FILE *file_ptr)
     fseek(file_ptr, 0, SEEK_END);                // seek to end of file
     file_len = ftell(file_ptr);
     rewind(file_ptr);
-    printf("File size: %lu\n", file_len);
     return file_len;
 }
 
@@ -30,15 +104,20 @@ int main(int argc, char *argv[])
     }
 
     FILE *fileptr;
-    unsigned long file_len;
     char *target_file_path = argv[1];
 
     fileptr = fopen(target_file_path, "rb");    // fopen in binary mode
-    if (fileptr)
-        file_len = get_file_size(fileptr);
-    else
+    if (!fileptr)
+    {
         printf("Error: file could not be opened for reading. Exiting.\n");
         return 0;
+    }
+
+    if (hexdump(fileptr) == 0)
+    {
+        fclose(fileptr);
+        return 0;
+    }
 
     fclose(fileptr);
     return 1;
